5c8cfa464c55db89a1636f11a2a326d2d23c48b4
VahidN <vahid_nasiri@yahoo.com>
2017-11-27 13:22:06 +0330

    Improved LoaderInterceptorService

--- src/app/core/interceptors/loader-interceptor.service.ts
+++ src/app/core/interceptors/loader-interceptor.service.ts
@@ -4,24 +4,42 @@ import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/do";
 import { SlimLoadingBarService } from "ng2-slim-loading-bar";
 
-
 @Injectable()
 export class LoaderInterceptorService implements HttpInterceptor {
+
+  private pendingRequests = 0;
+  private showLoading = false;
 
   constructor(private loadingBar: SlimLoadingBarService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // start our loader here
-    this.loadingBar.start();
+    this.pendingRequests++;
+    this.showLoadingBar();
+
     return next.handle(req).do(
       (event: HttpEvent<any>) => {
         if (event instanceof HttpResponse) {
-          this.loadingBar.complete();
+          this.hideLoadingBar();
         }
       },
       (err: any) => {
-        this.loadingBar.complete();
+        this.hideLoadingBar();
       });
+  }
+
+  private showLoadingBar() {
+    if (!this.showLoading) {
+      this.loadingBar.start();
+      this.showLoading = true;
+    }
+  }
+
+  private hideLoadingBar() {
+    this.pendingRequests--;
+    if (this.pendingRequests <= 0) {
+      this.loadingBar.complete();
+      this.showLoading = false;
+    }
   }
 }
